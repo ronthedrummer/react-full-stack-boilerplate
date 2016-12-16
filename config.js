04cfe77cc1cf808e2526cd76e82860eb37bc2a78
Ron McCranie <ron@ronmccranie.com>
2016-12-16 14:51:35 -0600

    Simple api router and index file

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ export const nodeEnv = env.NODE_ENV || 'development';
 
 export default {
   port: env.PORT || 8080
-}
+};
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-import http from 'http';
 import config from './config';
+import apiRouter from './api';
+import express from 'express';
 
-const server = http.createServer();
-server.listen(config.port);
+const server = express();
 
-console.log('node http server started on port '+config.port);
+server.use('/api',apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+  console.info('Express listening on port', config.port);
+});
